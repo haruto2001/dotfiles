1984eb58f7496a5934ca8ffe6ac129b86b010021
haruto2001 <tarako5515@gmail.com>
2023-06-23 16:45:40 +0900

    make setup.py available for Ubuntu

--- setup.sh
+++ setup.sh
@@ -2,7 +2,20 @@
 
 echo "Start setup."
 
-. ./install.sh
+# MacOSとUbuntuに対応
+if [ "$(uname)" == "Darwin" ]; then
+    # MacOSの場合
+    . ./install.sh
+elif [ -f /etc/os-release ] && grep -q "Ubuntu" /etc/os-release; then
+    # Ubuntuの場合
+    . ./install_for_ubuntu.sh
+else
+    # 未対応のOSまたはディストリビューションの場合
+    echo "This OS or distribution is not supported."
+    exit 1
+fi
+
+# シンボリックリンクの作成
 . ./link.sh
 
 echo "Setup is complete."
