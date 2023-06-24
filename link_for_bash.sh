897a495faeef911cc0439c558832b05f2197eb54
haruto2001 <tarako5515@gmail.com>
2023-06-24 14:42:35 +0900

    split link.sh for zsh and bash

---
+++ link_for_bash.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# 未定義な変数があったら途中で終了する
+set -eu
+
+# GirHub Actions用の設定
+if [ $CI = "true" ]; then
+  HOME="$HOME/work/dotfiles"
+fi
+
+# dotfilesディレクトリのPath
+DOTFILES_DIR="$HOME/dotfiles"
+# dotfilesディレクトリに移動する
+cd $DOTFILES_DIR
+
+echo "Creating symbolic links ..."
+# .から始まるファイルやフォルダに対して
+for f in .??*; do
+    # シンボリックリンクを作りたくないファイルやディレクトリを除外
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".DS_Store" ] && continue
+    [ "$f" = ".oh-my-zsh" ] && continue
+    [ "$f" = ".p10k.zsh" ] && continue
+    [ "$f" = ".vim" ] && continue
+
+    # シンボリックリンクを作成
+    ln -snfv ${DOTFILES_DIR}/$f $HOME
+done
+echo "Success!"
