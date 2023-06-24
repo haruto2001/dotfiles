c084d3f1d64bc597c6676c44c6a94682f6e005d8
haruto2001 <tarako5515@gmail.com>
2023-06-24 14:43:00 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,18 +4,25 @@ echo "Start setup."
 
 # MacOSとUbuntuに対応
 if [ "$(uname)" == "Darwin" ]; then
-    # MacOSの場合
-    . ./install_for_mac.sh
+  # MacOSの場合
+  . ./install_for_mac.sh
 elif [ -f /etc/os-release ] && grep -q "Ubuntu" /etc/os-release; then
-    # Ubuntuの場合
-    . ./install_for_ubuntu.sh
+  # Ubuntuの場合
+  . ./install_for_ubuntu.sh
 else
-    # 未対応のOSまたはディストリビューションの場合
-    echo "This OS or distribution is not supported."
-    exit 1
+  # 未対応のOSまたはディストリビューションの場合
+  echo "This OS or distribution is not supported."
+  exit 1
 fi
 
 # シンボリックリンクの作成
-. ./link.sh
+if [ $SHELL == "$(command -v zsh)" ]; then
+  . ./link_for_zsh
+elif [ $SHELL == "$(command -v bash)" ]; then
+  . ./link_for_bash.sh
+else
+  echo "This shell is not supported."
+  exit 1
+fi
 
 echo "Setup is complete."
